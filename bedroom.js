580bc79670344dce5cd48dc805668cad86c80b40
RJGinny <92090042+RJGinny@users.noreply.github.com>
2022-03-22 15:54:30 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,46 @@
+img = "";
+objstatus = "";
+objects = [];
+
+function preload()
+{
+img = loadImage('bedroom.jpeg');
+
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw()
+{
+image(img, 0, 0, 640, 420);
+if(objstatus !="")
+{
+    for (i = 0; i < objects. length; i++)
+    {
+        document.getElementById("status").innerHTML = "status : Object Detected"
+        document.getElementById("s1").innerHTML = "THERE ARE 4 BIG OBJECTS OUT OF WHICH "+ objects.length+" ARE DETECTED"
+        fill("#FF0000")
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+}
+function modelLoaded() {
+    console.log("Model Loaded!")
+    objstatus = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+
+    }
+    console.log(results);
+objects = results;
+}
